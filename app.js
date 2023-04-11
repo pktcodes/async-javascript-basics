d64de94f7d92a7451867512ee8bdfdcfb0fc7505
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-10 23:48:59 -0500

    Synchronous

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("async javascript");
+/* JavaScript is single threaded, synchronous language */
+
+console.log("I'm first");
+console.log("I'm second");
+console.log("I'm third");
+
+console.log("I'm first");
+boilingWater();
+console.log("I'm third");
+
+function boilingWater() {
+  console.log("boiling...");
+  for (let i = 0; i < 100000; i++) {
+    console.log("still boiling...");
+  }
+  console.log("done.");
+}
